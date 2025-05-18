06fd2d7fe705027cd67f0c61f42ef20a1b496732
soumya773 <150830999+soumya773@users.noreply.github.com>
2025-05-18 09:30:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //your JS code here. If required.
+const form = document.getElementById("voteForm");
+
+    form.addEventListener("submit", function(event) {
+      event.preventDefault(); // prevent actual form submission
+
+      const name = document.getElementById("name").value.trim();
+      const age = document.getElementById("age").value.trim();
+
+      // Validation
+      if (name === "" || age === "") {
+        alert("Please enter valid details.");
+        return;
+      }
+
+      // Promise
+      new Promise((resolve, reject) => {
+        setTimeout(() => {
+          if (parseInt(age) > 18) {
+            resolve(name);
+          } else {
+            reject(name);
+          }
+        }, 4000); // 4 seconds delay
+      })
+      .then((name) => {
+        alert(`Welcome, ${name}. You can vote.`);
+      })
+      .catch((name) => {
+        alert(`Oh sorry ${name}. You aren't old enough.`);
+      });
+    });
